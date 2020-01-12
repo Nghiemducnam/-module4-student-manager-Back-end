a55f1258b96bd49495e838ddd7dc6b024325272b
Nghiemducnam <Nghiemducnam0312@gmail.com>
2020-01-12 11:13:00 +0700

    create student in StudentController

--- src/main/java/com/codegym/controllers/StudentController.java
+++ src/main/java/com/codegym/controllers/StudentController.java
@@ -1,4 +1,35 @@
 package com.codegym.controllers;
-@R
+
+import com.codegym.models.Student;
+import com.codegym.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    @GetMapping("/api/student/")
+    public ResponseEntity<List<Student>> getStudentList(){
+        List<Student> studentList = studentService.findAllStudent();
+        if(studentList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(studentList, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/student/")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student, UriComponentsBuilder ucBuilder){
+        studentService.saveStudent(student);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/api/student/{id}").buildAndExpand(student.getStudentId()).toUri());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
 }
